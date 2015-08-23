5d271ea6f15e79cd1d282f5f8cae670319007ba4
Ntrancha <101010.nk@gmail.com>
2015-08-23 06:34:49 +0200

    ft_fileindos.c

---
+++ test.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ntrancha <ntrancha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/08/22 22:17:08 by ntrancha          #+#    #+#             */
+/*   Updated: 2015/08/23 06:34:43 by ntrancha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/libft.h"
+
+int     main(int argc, char **argv)
+{
+    char    **tab;
+    t_fdos  *list;
+    int     index;
+
+    list = ft_getdir_rec("/", NULL);
+    tab = ft_listtotab(list);
+    index = 0;
+    while (tab[index])
+        ft_putendl(tab[index++]);
+    ft_tabstrdel(tab);
+    ft_listdel(list, ft_memdel);
+}
